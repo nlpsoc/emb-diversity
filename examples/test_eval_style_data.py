65299a1c8e2f725e44373d827a98fdce3f63f8db
Anna <anna@porinzip.com>
2025-09-10 16:20:21 +0200

    - add variety/balance/disparity based on synthstel

--- examples/test_eval_style_data.py
+++ examples/test_eval_style_data.py
@@ -10,7 +10,7 @@ class TestOnSTEL:
             Test the evaluate_measures function on style datasets
         """
         # Load more and less diverse formal/informal dataset
-        from measure_diversity.eval.data.style import create_formal_diverse, create_formal_only
+        from measure_diversity.eval.data.STEL import create_formal_diverse, create_formal_only
         from measure_diversity.embeddings.SBERT import encode_style_sentences, encode_semantic_sentences
         only_formal_text = create_formal_only()
         half_formal_text = create_formal_diverse(formal_share=0.5)
@@ -42,3 +42,78 @@ class TestOnSTEL:
             [only_formal_semantic_vectors, half_formal_semantic_vectors], measures,
             dataset_names=["only formal semantic", "half formal half informal semantic"],)
 
+    def test_variety(self):
+        from measure_diversity.eval.data.synthstel import get_low_variety, get_high_variety
+        from measure_diversity.embeddings.SBERT import encode_style_sentences, encode_semantic_sentences
+        low_variety = get_low_variety()
+        high_variety = get_high_variety()
+
+        measures = [
+            lambda data: mean_pairwise_distance(data, metric="euclidean"),
+            lambda data: mean_pairwise_distance(data, metric="cosine"),
+            lambda data: distance_dispersion(data, metric="cosine"),
+            cluster_inertia_diversity
+        ]
+
+        # Add names to lambda functions for better display
+        measures[0].__name__ = "mean_pairwise_euclidean"
+        measures[1].__name__ = "mean_pairwise_cosine"
+        measures[2].__name__ = "distance_dispersion_cosine"
+
+        low_variety_sv = encode_style_sentences(low_variety)
+        high_variety_sv = encode_style_sentences(high_variety)
+
+        evaluate_measures.evaluate_monotone_order(
+            [low_variety_sv, high_variety_sv], measures,
+            dataset_names=["low variety style", "high variety style"])
+
+
+    def test_balance(self):
+        from measure_diversity.eval.data.synthstel import get_low_balance, get_high_balance
+        from measure_diversity.embeddings.SBERT import encode_style_sentences, encode_semantic_sentences
+        low_balance = get_low_balance()
+        high_balance = get_high_balance()
+
+        measures = [
+            lambda data: mean_pairwise_distance(data, metric="euclidean"),
+            lambda data: mean_pairwise_distance(data, metric="cosine"),
+            lambda data: distance_dispersion(data, metric="cosine"),
+            cluster_inertia_diversity
+        ]
+
+        # Add names to lambda functions for better display
+        measures[0].__name__ = "mean_pairwise_euclidean"
+        measures[1].__name__ = "mean_pairwise_cosine"
+        measures[2].__name__ = "distance_dispersion_cosine"
+
+        low_balance_sv = encode_style_sentences(low_balance)
+        high_balance_sv = encode_style_sentences(high_balance)
+
+        evaluate_measures.evaluate_monotone_order(
+            [low_balance_sv, high_balance_sv], measures,
+            dataset_names=["low balance style", "high balance style"])
+
+    def test_disparity(self):
+        from measure_diversity.eval.data.synthstel import get_low_disparity, get_high_disparity
+        from measure_diversity.embeddings.SBERT import encode_style_sentences, encode_semantic_sentences
+        low_disparity = get_low_disparity()
+        high_disparity = get_high_disparity()
+
+        measures = [
+            lambda data: mean_pairwise_distance(data, metric="euclidean"),
+            lambda data: mean_pairwise_distance(data, metric="cosine"),
+            lambda data: distance_dispersion(data, metric="cosine"),
+            cluster_inertia_diversity
+        ]
+
+        # Add names to lambda functions for better display
+        measures[0].__name__ = "mean_pairwise_euclidean"
+        measures[1].__name__ = "mean_pairwise_cosine"
+        measures[2].__name__ = "distance_dispersion_cosine"
+
+        low_disparity_sv = encode_style_sentences(low_disparity)
+        high_disparity_sv = encode_style_sentences(high_disparity)
+
+        evaluate_measures.evaluate_monotone_order(
+            [low_disparity_sv, high_disparity_sv], measures,
+            dataset_names=["low disparity style", "high disparity style"])
