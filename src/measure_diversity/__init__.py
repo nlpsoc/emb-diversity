07fca54c28df339b3be911ff4e135dd0fb6b25a6
vmenan <themachinequest@gmail.com>
2026-03-02 22:44:21 +0100

    adding caching the embeddings to the file

--- src/measure_diversity/__init__.py
+++ src/measure_diversity/__init__.py
@@ -1,3 +1,5 @@
 from .measure import dummy_diversity, mean_pairwise_distance
+from ._cache import cached_encode, clear_cache
+from .encoders import encode_st, encode_hf, encode_style, encode_semantic, encode_simcse
 
 __all__ = ["dummy_diversity", "mean_pairwise_distance"]
